c89e238f2fef53ad8a77fa7c67cf1fb7041dd8f2
陳泓維 <tim5556618@gmail.com>
2018-01-04 02:23:44 +0800

    修改MailController、新增路由、新增mail/test.blade

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    //
+    public function getSend(){
+
+        $content='安安 你好 測試';
+        $data=['name'=>'大雄','content'=>$content,];
+
+        Mail::send('email.test', $data, function($message){
+            $message->subject('Laravel 5 Mail');
+            $message->to('tim5556618@gmail.com','大雄');
+            $message->from('f37c183652-b21d25@inbox.mailtrap.io', 'netadmin');
+        });
+
+        return "Email 已寄出";
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+
+
+Route::get('mail','MailController@getSend');
+
+
+
